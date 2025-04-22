22a0c17d4dce477b82d6e700eb610c21081c5674
adem096 <ademrouissi55@gmail.com>
2025-04-22 14:32:31 +0100

    updates 22-04

--- models/departmentSchema.js
+++ models/departmentSchema.js
@@ -8,7 +8,7 @@ const departmentSchema = new mongoose.Schema(
     },
     description: {
         type: String,
-        required: true
+        required: false
     },
     nbrEmployes: {
       type: Number ,
