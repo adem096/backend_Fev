7e81e70b9a5499c1406537eeb85f1bc133d65b30
adem096 <ademrouissi55@gmail.com>
2025-04-25 22:47:11 +0100

    updates 25-04

--- models/jobOfferSchema.js
+++ models/jobOfferSchema.js
@@ -1,39 +1,38 @@
 const mongoose = require("mongoose");
 const jobOfferSchema = new mongoose.Schema(
   {
-    
-    title: { 
-        type: String, 
-        require: true 
+    title: {
+      type: String,
+      require: true,
     },
     description: {
-        type : String, 
-        require: true 
+      type: String,
+      require: true,
     },
     datePublication: {
-        type : Date, 
-        default:Date.now
+      type: Date,
+      default: Date.now,
     },
     status: {
-        type: String,
-        enum: ['Open', 'Closed', 'Pending'],
-        default: 'Open',
+      type: String,
+      enum: ["Open", "Closed", "Pending"],
+      default: "Open",
     },
     department: {
       type: String,
-      enum: ['Logistique', 'Marketing', 'Ressources Humaines', 'Finance', 'IT', 'Sales'],
-  },
-  type: {
-    type: String,
-    enum: ['Full time', 'Part time'],
-},
-    candidats : [{type : mongoose.Schema.Types.ObjectId,ref: 'User'}],
-    candidatures: [{ type: mongoose.Schema.Types.ObjectId, ref: "Candidature" }]
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["Full time", "Part time"],
+    },
+    candidats: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    candidatures: [
+      { type: mongoose.Schema.Types.ObjectId, ref: "Candidature" },
+    ],
   },
   { timestamps: true }
 );
-
-
 
 const JobOffer = mongoose.model("JobOffer", jobOfferSchema);
 module.exports = JobOffer;
